413135c5c6ec0af1119dd0e7d0911228472da9a1
mathewb <mathew.bryan@radiuspaymentsolutions.com>
2021-07-07 17:19:43 +0100

    Added edit form

--- quizmanager/Quiz/forms.py
+++ quizmanager/Quiz/forms.py
@@ -14,3 +14,13 @@ class CreateQuestionForm(forms.ModelForm):
     class Meta:
         model = QuestionsModel
         fields = ['quiz', 'question_title', 'answer_1', 'answer_2','answer_3','answer_4','answer_5',]
+
+class EditQuizForm(forms.ModelForm):
+    class Meta:
+        model = QuizModel
+        fields = ['quiz_title',]
+
+class EditQuestionForm(forms.ModelForm):
+    class Meta:
+        model = QuestionsModel
+        fields = ['quiz', 'question_title', 'answer_1', 'answer_2','answer_3','answer_4','answer_5',]
