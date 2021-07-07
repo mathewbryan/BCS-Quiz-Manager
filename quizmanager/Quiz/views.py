b72f659564f8aa58bbe36a3062e84c810edb60d3
mathewb <mathew.bryan@radiuspaymentsolutions.com>
2021-07-07 17:20:56 +0100

    Add login restrictions to views

--- quizmanager/Quiz/views.py
+++ quizmanager/Quiz/views.py
@@ -8,7 +8,7 @@ from Quiz.models import *
 from Quiz.forms import *
 
 # Create your views here.
-class List( TemplateView):
+class List(LoginRequiredMixin, TemplateView):
     template_name = "Quiz/list.html"
     login_url = '/login/'
     redirect_field_name = 'Quiz/login.html'
@@ -19,7 +19,7 @@ class List( TemplateView):
         context['questions'] = QuestionsModel.objects.all()
         return context
 
-class Questions(TemplateView):
+class Questions(LoginRequiredMixin,TemplateView):
     template_name = "Quiz/questions.html"
     
     def get_context_data(self, **kwargs):
@@ -28,7 +28,7 @@ class Questions(TemplateView):
         context['questions'] = QuestionsModel.objects.filter(quiz=self.kwargs['quiz_id'])
 
         return context
-class CreateQuizFormView(TemplateView):
+class CreateQuizFormView(LoginRequiredMixin, TemplateView):
     template_name = "Quiz/add_quiz.html"
     form_classes = {'quiz_form': CreateQuizForm,
                     'question_form': CreateQuestionForm,
@@ -67,7 +67,54 @@ class CreateQuizFormView(TemplateView):
         }
         return render(request, 'Quiz/add_quiz.html', context)
 
+class EditQuizFormView(LoginRequiredMixin, TemplateView):
+    template_name = "Quiz/edit_quiz.html"
+    form_classes = {'edit_quiz_form': EditQuizForm,
+                    'edit_question_form': EditQuestionForm,
+                    }
+    
+    def get(self,request,*args,**kwargs):
+        quiz_id = self.kwargs['quiz_id']
+        quizes = QuizModel.objects.get(id=quiz_id)
+        questions = QuestionsModel.objects.filter(quiz=quiz_id)
+        edit_quiz_form = EditQuizForm(instance=quizes)
+        # edit_question_form = EditQuestionForm()
+        edit_question_formset_factory = formset_factory(EditQuestionForm, extra=10)
 
+    
+        context = {'edit_quiz_form': edit_quiz_form, 'edit_question_form':edit_question_formset_factory, 'quizes':quizes}
+        return render(request, 'Quiz/edit_quiz.html', context)
+
+    def post(self, request, *args, **kwargs):
+        edit_quiz_form = EditQuizForm()
+        edit_question_form = EditQuestionForm()
+
+        quiz_id = self.kwargs['quiz_id']
+
+        action = self.request.POST['action']
+    
+        if (action == 'add_quiz'):
+            edit_quiz_form = EditQuizForm(request.POST)
+            quiz_to_edit = QuizModel.objects.get(id=quiz_id)
+            if edit_quiz_form.is_valid():
+                edit_quiz_form = EditQuizForm(request.POST, instance=quiz_to_edit)
+                edit_quiz_form.save()
+                context = {'edit_quiz_form': edit_quiz_form}
+                # TODO redirect somewhere else when form saved
+                edit_quiz_form = EditQuizForm()
+                
+        elif (action == 'add_question'):
+            edit_question_form = EditQuestionForm(request.POST)
+            if edit_question_form.is_valid():
+                edit_question_form.save()
+                context = {'edit_question_form': edit_question_form}
+                # TODO redirect somewhere else when form saved
+                edit_question_form = EditQuestionForm()
+        context = {
+            'edit_quiz_form': edit_quiz_form,
+            'edit_question_form': edit_question_form,
+        }
+        return render(request, 'Quiz/edit_quiz.html', context)
 # def create_quiz_form(request):
 #     form = CreateQuizForm()
 #     form2 = CreateQuestionForm()
