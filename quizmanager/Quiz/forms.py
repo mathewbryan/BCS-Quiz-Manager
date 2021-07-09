8e324ff67e4d3f988e361d6f07e5bc5b8c977b94
mathewb <mathew.bryan@radiuspaymentsolutions.com>
2021-07-09 16:37:04 +0100

    Updated form elements

--- quizmanager/Quiz/forms.py
+++ quizmanager/Quiz/forms.py
@@ -6,21 +6,35 @@ from .models import *
   
 # create a ModelForm
 class CreateQuizForm(forms.ModelForm):
+    quiz_title = forms.CharField(widget=forms.TextInput(attrs={'size': '50'}))
     class Meta:
         model = QuizModel
         fields = ['quiz_title',]
 
 class CreateQuestionForm(forms.ModelForm):
+    question_title = forms.CharField(widget=forms.TextInput(attrs={'size': '75'}))
+    answer_1 = forms.CharField(widget=forms.TextInput(attrs={'size': '50'}))
+    answer_2 = forms.CharField(widget=forms.TextInput(attrs={'size': '50'}))
+    answer_3 = forms.CharField(required=False,widget=forms.TextInput(attrs={'size': '50'}))
+    answer_4 = forms.CharField(required=False,widget=forms.TextInput(attrs={'size': '50'}))
+    answer_5 = forms.CharField(required=False,widget=forms.TextInput(attrs={'size': '50'}))
     class Meta:
         model = QuestionsModel
         fields = ['quiz', 'question_title', 'answer_1', 'answer_2','answer_3','answer_4','answer_5',]
 
 class EditQuizForm(forms.ModelForm):
+    quiz_title = forms.CharField(widget=forms.TextInput(attrs={'size': '50'}))
     class Meta:
         model = QuizModel
         fields = ['quiz_title',]
 
 class EditQuestionForm(forms.ModelForm):
+    question_title = forms.CharField(widget=forms.TextInput(attrs={'size': '75'}))
+    answer_1 = forms.CharField(required=False,widget=forms.TextInput(attrs={'size': '50'}))
+    answer_2 = forms.CharField(required=False,widget=forms.TextInput(attrs={'size': '50'}))
+    answer_3 = forms.CharField(required=False,widget=forms.TextInput(attrs={'size': '50'}))
+    answer_4 = forms.CharField(required=False,widget=forms.TextInput(attrs={'size': '50'}))
+    answer_5 = forms.CharField(required=False,widget=forms.TextInput(attrs={'size': '50'}))
     class Meta:
         model = QuestionsModel
         fields = ['quiz', 'question_title', 'answer_1', 'answer_2','answer_3','answer_4','answer_5',]
